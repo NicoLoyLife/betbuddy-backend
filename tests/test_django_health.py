9693b9398fc5149b9de950a5601ff1974a808f8e
Nico Götze <n.goetze@timmehosting.de>
2025-07-22 16:24:57 +0200

    chore: entferne veralteten Django-Healthcheck-Test zugunsten von Integrationstest

--- tests/test_django_health.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from django.test import Client
-
-
-@pytest.mark.django_db
-def test_admin_login_page_accessible():
-    client = Client()
-    response = client.get("/admin/")
-    assert response.status_code == 200
